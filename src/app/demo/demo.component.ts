69f9366689b26e3c9998b4afc1613636b1e80d54
Ilya Naryshkin <inaryshkin@mentalstack.com>
2019-12-27 16:43:39 +0300

    db for test

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,18 +1,13 @@
 import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {DemoService} from './demo.service';
 
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.scss']
 })
 export class DemoComponent implements OnInit {
-
   public data: any[];
-  public days = Array(35).fill(undefined);
-
 
   // Filters
   public compSetOptions = compSetOptions;
@@ -34,65 +29,70 @@ export class DemoComponent implements OnInit {
 
   constructor(
     private demoService: DemoService,
+    private cdr: ChangeDetectorRef,
   ) {
   }
 
-  ngOnInit(): void {
-    this.demoService.getData().subscribe((data) => {
-      this.data = data;
-    });
+  async ngOnInit() {
+    await this.demoService.initDataFromServer();
   }
 
-  search() {
-    console.log('actions', this.compSet, this.source, this.price, this.pos, this.los, this.roomType, this.mealType)
+  async search(
+    year: number = 2019,
+    compset_id: number = 1,
+    provider_id: number = 2,
+    los: number = 3,
+  ) {
+    this.data = await this.demoService.find(year, compset_id, provider_id, los);
   }
 
   getDay() {
-    const day = new Date(this.data[0].year, this.data[0].month - 1).getDay();
-    return day;
+    return new Date(this.data[0].year, this.data[0].month - 1).getDay();
   }
-
-
 }
-
 
 const compSetOptions = [
   {id: 0, name: 'Low'},
   {id: 1, name: 'Median'},
   {id: 2, name: 'High'},
 ];
+
 const sourceOptions = [
   {id: 0, name: 'Booking.com'},
   {id: 1, name: 'Hotels.com'},
   {id: 2, name: 'Airbnb.com'},
   {id: 3, name: 'Trivago.com'},
 ];
+
 const priceOptions = [
   {id: 0, name: 'Best flex'},
 ];
+
 const posOptions = [
   {id: 0, name: 'EU'},
   {id: 1, name: 'UK'},
   {id: 2, name: 'USA'},
   {id: 3, name: 'RU'},
 ];
+
 const losOptions = [
   {id: 0, name: '1 Night'},
   {id: 1, name: '2 Night'},
   {id: 2, name: '5 Night'},
   {id: 3, name: '10 Night'},
 ];
+
 const roomTypeOptions = [
   {id: 0, name: 'All'},
   {id: 1, name: '1 type'},
   {id: 2, name: '2 type'},
   {id: 3, name: '3 type'},
 ];
+
 const mealTypeOptions = [
   {id: 0, name: 'All'},
   {id: 1, name: 'None'},
   {id: 2, name: 'Half'},
   {id: 3, name: 'Breakfast'},
   {id: 4, name: 'Full'},
-
 ];
--- src/app/demo/demo.service.ts
+++ src/app/demo/demo.service.ts
@@ -1,29 +1,73 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import * as Datastore from 'nedb';
 
 @Injectable()
 export class DemoService {
+  public days = Array(35).fill(undefined);
+  public db;
+
   constructor(
     private http: HttpClient
   ) {
+    this.db = new Datastore({ filename: 'calendar.json', autoload: true });
   }
 
+  public initDataFromServer(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.db.remove({}, { multi: true }, (err, numRemoved) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        }
+        // tslint:disable-next-line:no-console
+        console.info('numRemoved', numRemoved);
+        this.db.persistence.compactDatafile();
+        // this.db.compactDatafile();
 
-  public getData(): Observable<any> {
-    return this.http.post('https://ci-server-demo.herokuapp.com/structure-generate', {
-      year: 2019,
-      month: 10,
-      base_price: 100,
-      high_diff: {from: 20, to: 40 },
-      mid_diff: {from: 10, to: 15 },
-      low_diff: {from: -10, to: -15 },
-      no_of_pos: 3,
-      no_of_providers: 5,
-      no_of_rates: 2,
-      room_price: 100,
-      no_of_los: 3
+        this.http.post('https://ci-server-demo.herokuapp.com/structure-generate', {
+          year: 2019,
+          month: 10,
+          base_price: 100,
+          high_diff: {from: 20, to: 40 },
+          mid_diff: {from: 10, to: 15 },
+          low_diff: {from: -10, to: -15 },
+          no_of_pos: 3,
+          no_of_providers: 5,
+          no_of_rates: 2,
+          room_price: 100,
+          no_of_los: 3
+        }).subscribe((data) => {
+          this.db.insert(data);
+          // tslint:disable-next-line:no-console
+          console.info('inserted data', data);
+          resolve();
+        });
+      });
     });
   }
 
+  public async find(
+    year: number = 2019,
+    compset_id: number = 1,
+    provider_id: number = 2,
+    los: number = 3,
+  ): Promise<any[]> {
+    return new Promise((resolve, reject) => {
+      this.db.find({
+        year,
+        compset_id,
+        provider_id,
+        los,
+      }).skip(0).limit(10).exec((err, data) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        }
+
+        console.log('data', data);
+        resolve(data);
+      });
+    });
+  }
 }
