3fc32da324b735490ce3fe0e9b8f8edc2740c7a6
Tarasov Oleg <otarasov@mentalstack.com>
2019-12-27 18:14:27 +0300

    comp prices

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,5 +1,15 @@
 import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {DemoService} from './demo.service';
+import {find} from 'lodash';
+
+interface IData {
+  year: string;
+  month: string;
+  compset_id: number;
+  provider_id: number;
+  los: number;
+  checkin_dates: any[];
+}
 
 @Component({
   selector: 'app-root',
@@ -7,8 +17,9 @@ import {DemoService} from './demo.service';
   styleUrls: ['./demo.component.scss']
 })
 export class DemoComponent implements OnInit {
-  public data: any[];
+  public data: IData;
   public days = Array(35).fill(undefined);
+  public fetching = false;
 
   // Filters
   public compSetOptions = compSetOptions;
@@ -35,15 +46,42 @@ export class DemoComponent implements OnInit {
   }
 
   async ngOnInit() {
+    this.fetching = true;
     await this.demoService.initDataFromServer();
+    this.fetching = false;
   }
 
   async search() {
-    this.data = await this.demoService.find(this.compSet, this.source, this.los);
+    [this.data] = await this.demoService.find(this.compSet, this.source, this.los);
+
+    this.data.checkin_dates = this.data.checkin_dates.map((el, i) => {
+      const customerRates = find(el.room_types.customer_rates, { room_rate_name_id: this.roomType, meal_plan: this.mealType  });
+      const competitorRates = find(el.room_types.competitors_rates, { room_rate_name_id: this.roomType, meal_plan: this.mealType  });
+
+      return {
+        ...el,
+        room_types: {
+          ...el.room_types,
+          customer_rates: customerRates,
+          competitors_rates: competitorRates
+        },
+      };
+    });
+
+    console.log('lodash data', this.data)
+
+  }
+
+  comparePrices(roomTypes) {
+
+    const cusPrice = roomTypes.customer_rates.price;
+    const comPrice = roomTypes.competitors_rates.price;
+
+    return Math.round((comPrice - cusPrice) / cusPrice * 100);
   }
 
   getDay() {
-    return new Date(this.data[0].year, this.data[0].month - 1).getDay();
+    return new Date(Number(this.data.year), Number(this.data.month) - 1).getDay();
   }
 }
 
@@ -79,16 +117,14 @@ const losOptions = [
 ];
 
 const roomTypeOptions = [
-  {id: 0, name: 'All'},
-  {id: 1, name: '1 type'},
-  {id: 2, name: '2 type'},
-  {id: 3, name: '3 type'},
+  {id: 0, name: '1 type'},
+  {id: 1, name: '2 type'},
+  {id: 2, name: '3 type'},
 ];
 
 const mealTypeOptions = [
-  {id: 0, name: 'All'},
-  {id: 1, name: 'None'},
-  {id: 2, name: 'Half'},
-  {id: 3, name: 'Breakfast'},
-  {id: 4, name: 'Full'},
+  {value: 'NONE', name: 'None'},
+  {value: 'HALF', name: 'Half'},
+  {value: 'BREAKFAST', name: 'Breakfast'},
+  {value: 'FULL', name: 'Full'},
 ];
